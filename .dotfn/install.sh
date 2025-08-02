c4c8d555df0e8ed21110294f3d91e33f2de5f194
Nicolas <169418539+dotfn@users.noreply.github.com>
2025-08-01 21:16:02 -0300

    minimal setup

--- .dotfn/install.sh
+++ .dotfn/install.sh
@@ -1,5 +1,75 @@
 #!/bin/bash
-
-echo $PATH
+export PATH="/usr/local/bin:/usr/bin:/bin:/usr/local/sbin:/usr/sbin:/sbin"
 
 sudo pacman -Syy
+
+###System 
+sudo pacman -S \
+	git \
+	wget \
+	eza \
+	zsh \
+	neovim \
+	openssh \
+
+###hyprland base
+sudo pacman -S  \
+	hyprland \
+	ly \
+	uwsm \
+	kitty \
+	xdg-desktop-portal-gtk \
+	xdg-desktop-portal-hyprland \
+	xdg-user-dirs \
+	hyprpolkitagent 
+
+### hyprland ecosystem
+sudo pacman -S hyprsunset
+
+# ly config 
+sudo systemctl enable ly.service
+sudo systemctl disable getty@tty2.service
+
+# enable autostart hypr polkit
+systemctl --user enable --now hyprpolkitagent.service
+
+# enable sunset
+systemctl --user enable --now hyprsunset.service
+
+###----------------------------
+### Yazi file explorer
+###----------------------------
+sudo pacman -S yazi ffmpeg 7zip jq poppler fd ripgrep fzf zoxide resvg imagemagick
+
+# Yazi plugins
+ya pkg add yazi-rs/plugins:mount
+
+###----------------------------
+### oh-my-zsh
+###----------------------------
+
+RUNZSH=no CHSH=no KEEP_ZSHRC=yes \
+sh -c "$(wget -qO- https://raw.githubusercontent.com/ohmyzsh/ohmyzsh/master/tools/install.sh)"
+
+git clone https://github.com/zsh-users/zsh-autosuggestions \
+	${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-autosuggestions
+git clone https://github.com/zsh-users/zsh-syntax-highlighting.git \
+	${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-syntax-highlighting
+git clone https://github.com/zsh-users/zsh-completions \
+	${ZSH_CUSTOM:=~/.oh-my-zsh/custom}/plugins/zsh-completions
+git clone https://github.com/zsh-users/zsh-history-substring-search \
+	${ZSH_CUSTOM:-~/.oh-my-zsh/custom}/plugins/zsh-history-substring-search
+
+#cross-shell prompt
+sudo pacman -S starship
+
+#Apps
+sudo pacman -S \
+	ttf-input-nerd \
+	firefox	\
+	mpv \
+	bluetui \
+
+##
+chsh -s $(which zsh)
+
