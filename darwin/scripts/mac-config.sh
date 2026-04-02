a30cefa34649c02f74a4685069e1767066f68b66
dotfn <dev@dotfn.local>
2026-04-02 07:52:14 +0000

    feat(darwin): update mac-config script to enhance Finder and Dock settings

--- darwin/scripts/mac-config.sh
+++ darwin/scripts/mac-config.sh
@@ -1,3 +1,5 @@
 #!/bin/bash
 defaults write com.apple.finder AppleShowAllFiles true && killall Finder
+defaults write com.apple.finder ShowPathbar -bool true && killall Finder
+defaults write com.apple.dock autohide-delay -float 0 && defaults write com.apple.dock autohide-time-modifier -float 0.4 && killall Dock
 touch ~/.hushlogin
